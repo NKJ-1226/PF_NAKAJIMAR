064663b220fb58a8bcbb37107428721e973d4431
YourGitHubUsername <YourEmail@example.com>
2025-03-01 16:30:35 +0900

    コミットしたよ

---
+++ src/main/java/com/example/demo/config/BeanDefine.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanDefine {
+
+    @Bean
+    Mapper mapper() {
+	    return new DozerBeanMapper();
+	}
+
+//    @Bean
+//    PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+}
